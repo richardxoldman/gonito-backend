d88e20d7ed8d8385c464d7eb10f3df769fc95196
Filip Gralinski <filipg@amu.edu.pl>
2015-09-06 14:24:49 +0200

    show Readme

--- Handler/ShowChallenge.hs
+++ Handler/ShowChallenge.hs
@@ -2,9 +2,30 @@ module Handler.ShowChallenge where
 
 import Import
 
+import qualified Data.Text.Lazy          as TL
+import           Text.Markdown
+
+import Handler.Extract
+import Handler.Shared
+
 getShowChallengeR :: Text -> Handler Html
 getShowChallengeR name = do
   (Entity _ challenge) <- runDB $ getBy404 $ UniqueName name
+  challengeLayout True challenge (showChallengeWidget challenge)
+
+getChallengeReadmeR :: Text -> Handler Html
+getChallengeReadmeR name = do
+  (Entity _ challenge) <- runDB $ getBy404 $ UniqueName name
+  let repoId = challengePublicRepo challenge
+  let repoDir = getRepoDir repoId
+  let readmeFilePath = repoDir </> readmeFile
+  contents <- readFile readmeFilePath
+  challengeLayout False challenge $ toWidget $ markdown def $ TL.fromStrict contents
+
+showChallengeWidget challenge = $(widgetFile "show-challenge")
+
+challengeLayout withHeader challenge widget = do
+  bc <- widgetToPageContent widget
   defaultLayout $ do
-    setTitle "Show a challenge"
-    $(widgetFile "show-challenge")
+    setTitle "Challenge"
+    $(widgetFile "challenge")
