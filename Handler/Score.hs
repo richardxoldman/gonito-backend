ea217896be5c793592ac3a18443c583685f01559
Filip Gralinski <filipg@amu.edu.pl>
2017-05-15 14:25:54 +0200

    score table usable now

--- Handler/Score.hs
+++ Handler/Score.hs
@@ -5,12 +5,16 @@ import Import
 import Handler.Shared
 import Handler.Tables
 
+import Control.Monad.Extra
+
 import Handler.AchievementUtils
 
 import qualified Database.Esqueleto      as E
 import           Database.Esqueleto      ((^.))
 
 import qualified Yesod.Table as Table
+
+import Data.Text as T
 
 getMyScoreR :: Handler Html
 getMyScoreR = do
@@ -22,12 +26,18 @@ getScoreR userId = do
   user <- runDB $ get404 userId
   doScore (Entity userId user)
 
-scoreTable :: Table.Table App (AchievementInfo, Entity Submission)
+scoreTable :: Table.Table App (AchievementInfo, (Entity Submission, Bool))
 scoreTable = mempty
   ++ Table.text "name" (achievementInfoName . fst)
   ++ achievementDescriptionCell fst
   ++ timestampCell "deadline" (achievementInfoDeadline . fst)
-  ++ Table.text "submission" (submissionDescription . entityVal . snd)
+  ++ timestampCell "submitted" (submissionStamp . entityVal . fst . snd)
+  ++ Table.linked "submission" (submissionDescription . entityVal . fst . snd) (EditSubmissionR . entityKey . fst . snd)
+  ++ Table.text "status" getStatus
+
+getStatus :: (AchievementInfo, (Entity Submission, Bool)) -> Text
+getStatus (_, (_, False)) = ""
+getStatus (aInfo, (_, True)) = T.pack $ show $ achievementInfoPoints aInfo
 
 doScore :: Entity User -> Handler Html
 doScore (Entity userId user) = do
@@ -45,7 +55,20 @@ doScore (Entity userId user) = do
     setTitle "Score"
     $(widgetFile "score")
 
-processEntry :: Entity User -> (Entity Achievement, Entity Submission) -> Handler (AchievementInfo, Entity Submission)
+processEntry :: Entity User -> (Entity Achievement, Entity Submission) -> Handler (AchievementInfo, (Entity Submission, Bool))
 processEntry entUser (entAchievement, entSubmission) = do
   aInfo <- runDB $ getAchievementInfo (Just entUser) entAchievement
-  return (aInfo, entSubmission)
+
+  accepted <- allM (checkSubmissionTag entSubmission) (achievementInfoTags aInfo)
+
+  return (aInfo, (entSubmission, accepted))
+
+
+checkSubmissionTag :: Entity Submission -> Entity Tag -> Handler Bool
+checkSubmissionTag (Entity submissionId _) (Entity tagId _) = do
+  mSubmissionTag <- runDB $ getBy $ UniqueSubmissionTag submissionId tagId
+  return $ case mSubmissionTag of
+    Just (Entity _ submissionTag) -> case submissionTagAccepted submissionTag of
+      Just b -> b
+      Nothing -> False
+    Nothing -> False
