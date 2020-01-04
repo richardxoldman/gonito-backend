c9b8f659580bf8b7230529cfe9e1ac12b602914d
Filip Gralinski <filipg@amu.edu.pl>
2020-01-04 22:36:12 +0100

    Add helper script

---
+++ sql-scripts/add-indexes.sql
@@ -0,0 +1,5 @@
+create index evaluation_checksum_idx on evaluation(checksum);
+create index out_variant_idx on out(variant);
+create index submission_challenge_idx on submission(challenge);
+create index variant_submission_idx on variant(submission);
+create index out_checksum_idx on out(checksum);
