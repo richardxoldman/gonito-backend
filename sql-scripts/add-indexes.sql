ab5e03483f3a57f00749fc486d90e7d0a38b6b5b
Filip Gralinski <filipg@amu.edu.pl>
2021-01-25 10:39:03 +0100

    Improve Postgres indexes

--- sql-scripts/add-indexes.sql
+++ sql-scripts/add-indexes.sql
@@ -3,3 +3,4 @@ create index out_variant_idx on out(variant);
 create index submission_challenge_idx on submission(challenge);
 create index variant_submission_idx on variant(submission);
 create index out_checksum_idx on out(checksum);
+create index achievement_challenge_idx on achievement(challenge);
