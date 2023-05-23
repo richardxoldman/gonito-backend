d1b1a37894b7a7862e04fa185767f39788d80bf8
flaizdnag <andrzej.m.gajda@gmail.com>
2023-05-23 12:55:33 +0200

    code refactoring

--- Handler/DeleteSubmission.hs
+++ Handler/DeleteSubmission.hs
@@ -1,45 +1,15 @@
-{-# LANGUAGE TupleSections, OverloadedStrings, RankNTypes #-}
-
 module Handler.DeleteSubmission where
 
-import           Import               hiding (Proxy, encodeUtf8, fromList)
-
-import           Handler.MakePublic
-
-import           Yesod.Form.Bootstrap3      (BootstrapFormLayout (..), bfs,
-                                             renderBootstrap3)
-
-import qualified Data.ByteString            as S
-import qualified Data.ByteString.Lazy       as L
-import           Data.Conduit.Binary
-
-import           System.Directory
-import           System.IO
-import           System.Process
-
-import           Handler.Common             (isPasswordAcceptable,
-                                             passwordConfirmField,
-                                             tooWeakPasswordMessage,
-                                             updatePassword)
-import           Handler.JWT
-import           Handler.Shared
-
-import           Data.Swagger               hiding (delete, get, tags)
-import qualified Data.Swagger               as DS
+import           Import                     hiding (Proxy, encodeUtf8, fromList)
 
 import           Control.Lens               hiding ((.=), (^.))
+import           Data.HashMap.Strict.InsOrd (fromList)
 import           Data.Proxy                 as DPR
+import           Data.Swagger               hiding (delete, get, tags)
+import qualified Data.Swagger               as DS
 import           Data.Swagger.Declare
 
-import           Data.Aeson                 hiding (Key)
-import           Data.Aeson.Key             (fromText)
-import           Data.Aeson.KeyMap          hiding (delete, filter, foldr,
-                                             fromList, insert, map, null)
-
-import           Data.HashMap.Strict.InsOrd (fromList)
-
-import           Data.Text                  (strip)
-import           Data.Text.Encoding         (encodeUtf8)
+import           Handler.MakePublic
 
 postDeleteSubmissionR :: SubmissionId -> Handler ()
 postDeleteSubmissionR submissionId = do
@@ -58,18 +28,16 @@ declareDeleteSubmissionApi = do
     response <- declareResponse (Proxy :: Proxy String)
 
     pure $ mempty
-        & paths .~ fromList [
-            ("/api/delete-submission/{submissionId}"
-            , mempty & DS.post ?~
-                ( mempty
-                & parameters .~ [
-                    Inline $ mempty
-                        & name .~ "submissionId"
-                        & required ?~ True
-                        & schema .~ ParamOther (mempty
-                            & in_ .~ ParamPath
-                            & paramSchema .~ idSchema)
-                    ]
+        & paths .~ fromList
+            [ ("/api/delete-submission/{submissionId}", mempty
+                & DS.post ?~ (mempty
+                & parameters .~ [ Inline $ mempty
+                    & name .~ "submissionId"
+                    & required ?~ True
+                    & schema .~ ParamOther (mempty
+                        & in_ .~ ParamPath
+                        & paramSchema .~ idSchema)
+                ]
                 & produces ?~ MimeList ["application/json"]
                 & description ?~ "Deletes the submission by setting 'true' value in the deleted field."
                 & at 200 ?~ Inline response ))
